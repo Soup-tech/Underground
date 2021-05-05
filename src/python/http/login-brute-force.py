ce4de1eb0c54c5278b8f0cfa82974b01bf0562a1
soup-tech <campbellssoup21@gmail.com>
2021-05-05 02:04:12 -0400

    http python tools

--- src/python/http/login-brute-force.py
+++ src/python/http/login-brute-force.py
@@ -2,19 +2,47 @@
 
 import http.client
 import urllib.parse
-
-serv_ip = input("Host/IP: ")
-serv_port = input("Port: ")
-user_wordlist = input("Username Wordlist: ")
-password_wordlist = input("Password Wordlist: ")
-
-connection = http.client.HTTPConnection(serv_ip,serv_port)
-
-for user in open(user_wordlist,'r'):
-	user = user.strip()
-	for password in open(password_wordlist, 'r'):
-		password = password.strip()
+import sys
 
 
-		
-		
+def main():
+	if (len(sys.argv) == 3):
+		print("Usage: ./login-brute-force [user wordlist] [password wordlist]")
+		exit(1)
+
+	serv_host = input("host/IP : ")
+	serv_port = input("port : ")
+
+	lst = serv_host.split("/")
+
+	if (len(lst) < 3):
+		url = '/'
+	else:
+		url = extension(serv_host)	
+
+	print(url)	
+	serv_ip = serv_host[2]
+
+	# check if server is up
+	connection = http.client.HTTPConnection(host, serv_port)
+	connection.request('GET','/')	
+	pirnt(connection.status)
+
+
+	# Drive loop
+	#for user in open(sys.argv[1], 'r', encoding='utf-8'):
+	#	user = user.strip()
+	#	for password in open(sys.argv[2], 'r' encoding='utf-8'):
+	#		password = password.strip()
+
+
+def extension(serv_host):
+	lst = serv_host.split('/')
+	url = "/" + "/".join(lst[3:])
+	return url
+
+
+
+
+if __name__=="__main__":
+	main()
