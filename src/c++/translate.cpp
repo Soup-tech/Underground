0881fbc6a28efecdb922d16f589eab45d056230c
soup-tech <campbellssoup21@gmail.com>
2021-05-07 00:26:24 -0400

    Writing translator

--- src/c++/translate.cpp
+++ src/c++/translate.cpp
@@ -1,33 +1,24 @@
-#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <cstring>
 
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#include <dirent.h>
-
 int main() {
 
-	int sockfd, new_sockfd, recv_length;
+	int sockfd, new_sockfd, recv_length = 1;
 	struct sockaddr_in serv_addr, client_addr;
-	struct dirent *dent;
-	DIR *dr;
-	socklen_t sin_size;
+	char buffer[1024];
 
-	dr = opendir("/");
-
-	puts("Creating Socket...");
+	puts("Creating socket...");
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) {
 		puts("Error: Could not create socket");
 		exit(1);
 	}
-	puts("Socket was created");
+	puts("Socket Created");
 
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
@@ -36,39 +27,49 @@ int main() {
 
 	puts("Binding Socket...");
 	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) == -1) {
-		puts("Error: Could not bind to port");
+		puts("Error: Could not bind socket");
 		exit(1);
 	}
-	puts("Socket has binded");
+	puts("Binded Socket");
+
+	puts("Listening....");
+	listen(sockfd, 3);
 
 	while (1) {
-		puts("Listening...");
-		if (listen(sockfd, 3) == -1) {
-			puts("Error: Socket could not listen");
-			exit(1);
-		}
 
 		sin_size = sizeof(struct sockaddr_in);
 		new_sockfd = accept(sockfd, (struct sockaddr *) &client_addr, &sin_size);
 		if (new_sockfd == -1) {
-			puts("Error: Could not except connection");
+			puts("Error: Could not accept connection");
 			exit(1);
 		}
-		puts("Accepted connection");		
 		
-		while (1) {	
-			char buffer[1024];
-
-			if (strcmp(buffer, "exit\r\n") == 0) {
-				close(new_sockfd);
-				memset(&buffer, 0, 1024);
-				break;
-			} 
-			
-			recv_length = recv(new_sockfd, buffer, 1024, 0);
-			printf("%s\n", buffer);
+		while (recv_length > 0) {
+			printf("RECV: %D bytes\n", recv_length);
+			dump(buffer, recv_length);
+			recv_length = recv(new_sockfd, &buffer, 1024, 0);
 		}
-
+		close(new_sockfd);
 	}
+
 	return 0;
 }
+
+void dump(const unsigned char *data_buffer, const unsigned int length) {
+
+	unsigned char byte;
+	unsigned int i, j;
+	for (i = 0 ; i < length ; i++) {
+		byte = data_buffer
+	}
+
+
+
+
+
+
+
+
+}
+
+
